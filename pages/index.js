082945be8ed3bf2b9c3d9666b50800e5c499c11a
Esteban Rao <estebanrao@gmail.com>
2020-07-24 12:35:31 -0300

    Adds homepage redirect to country automatically

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <h1>Please select a country</h1>;
-}
+import Router from 'next/router';
+
+import axios from 'axios';
+
+const Home = () => null;
+
+Home.getInitialProps = async context => {
+  const ipDataResponse = await axios.get(
+    `https://api.ipdata.co?api-key=${process.env.IPDATA_API_KEY}`
+  );
+  const country = ipDataResponse.data?.country_code?.toLowerCase() || 'us';
+
+  process.browser
+    ? Router.replace('/[country]', `${country}`)
+    : context.res.writeHead(302, { Location: `/${country}` });
+
+  context.res.end();
+};
+
+export default Home;
